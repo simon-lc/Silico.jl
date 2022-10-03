2952d0a010a9794df4df6de88f4832c0f7669a42
simon-lc <simonlc@stanford.edu>
2022-10-02 22:48:23 -0700

    remove cvx

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,11 @@
 function module_dir()
     return joinpath(@__DIR__, "..")
 end
+
+function normalize_A!(A::Matrix; ϵ=1e-6)
+	nh = size(A,1)
+	for i = 1:nh
+		A[i,:] ./= norm(A[i,:]) + ϵ
+	end
+	return nothing
+end
